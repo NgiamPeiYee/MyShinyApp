985a7ccf06cd02045db388004b55560a04489a35
NgiamPeiYee <67258621+NgiamPeiYee@users.noreply.github.com>
2021-06-13 17:20:01 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,32 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Miles per Gallon"),
+   
+    sidebarPanel(
+      
+      selectInput("variable", "Variable:", 
+                  c("Cylinders" = "cyl",
+                    "Transmission" = "am",
+                    "Gears" = "gear"))
+    ),
+
+      
+    mainPanel(
+            
+            h3(textOutput("caption")),
+            plotOutput("mpgPlot")
+        )
+    ))
